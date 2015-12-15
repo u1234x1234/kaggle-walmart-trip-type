2d11f7baf8df46e5fdb8fdece28c76eb81ee94c0
u1234x1234 <u1234x1234@gmail.com>
2015-12-16 04:57:49 +0500

    50it 786

--- cpp/feature_extractor/main.cpp
+++ cpp/feature_extractor/main.cpp
@@ -11,7 +11,7 @@ using namespace cv;
 string root_path = "/home/dima/walmart/";
 unordered_map<string, int> feature_to_id[6];
 vector<int> feat_count(6);
-
+unordered_map<string,int> week_day_mapping = {{"Monday", 0}, {"Tuesday", 1}, {"Wednesday", 2}, {"Thursday", 3}, {"Friday", 4}, {"Sunday", 5}, {"Saturday", 6}};
 void read_test_stat()
 {
 	vector<string> header;
@@ -84,7 +84,8 @@ map<int,trip_info> read_trips(string mode = "train")
 		int trip_type = stoi(tokens[0]);
 		int visit_number = stoi(tokens[1 - fl]);
 
-		int weekday = feature_to_id[1][tokens[2 - fl]];
+//		int weekday = feature_to_id[1][tokens[2 - fl]];
+		int weekday = week_day_mapping[tokens[2 - fl]];
 
 		int dat[4];
 		for (int i = 2; i < 6; i++)
@@ -130,29 +131,35 @@ void gen_features(const map<int,trip_info> &trips, string mode = "train")
 		write(trip.second.weekday, 1);
 
 		int ret_flag = 0;
-		int nan_flag = 0;
+		int nan_flag1 = 0;
+		int nan_flag2 = 0;
+		int nan_flag3 = 0;
 		int scan_count = 0;
+		int ret_count = 0;
 
-		map<int, int> hist[3];
+		map<int, int> hist[4];
 
 		for (size_t i = 0; i < trip.second.data[0].size(); i++)
 		{
 			if (trip.second.data[1][i] < 0) // return
 			{
 				ret_flag++;
+				ret_count += trip.second.data[1][i];
 			}
 			else
 			{
 				hist[0][trip.second.data[0][i] + 7] += trip.second.data[1][i];
-				hist[1][trip.second.data[2][i] + 7 + 98148]++;
-				hist[2][trip.second.data[3][i] + 7 + 98148 + 68]++;
+				hist[1][trip.second.data[2][i] + 7 + 98148] += trip.second.data[1][i];
+				hist[2][trip.second.data[3][i] + 7 + 98148 + 68] += trip.second.data[1][i];
 				scan_count += trip.second.data[1][i];
 			}
 
+			if (trip.second.data[0][i] == 563)
+				nan_flag1++;
 			if (trip.second.data[2][i] == 46)
-				nan_flag++;
+				nan_flag1++;
 			if (trip.second.data[3][i] == 418)
-				nan_flag++;
+				nan_flag1++;
 		}
 		for (int i = 0; i < 3; i++)
 			for (auto &it : hist[i])
@@ -162,8 +169,17 @@ void gen_features(const map<int,trip_info> &trips, string mode = "train")
 
 		write(7 + 98148 + 68 + 5204, ret_flag);
 		write(7 + 98148 + 68 + 5204 + 1, trip.second.data[0].size());
-		write(7 + 98148 + 68 + 5204 + 2, nan_flag);
-		write(7 + 98148 + 68 + 5204 + 3, scan_count);
+		write(7 + 98148 + 68 + 5204 + 2, nan_flag1);
+//		write(7 + 98148 + 68 + 5204 + 3, nan_flag2);
+//		write(7 + 98148 + 68 + 5204 + 4, nan_flag3);
+		write(7 + 98148 + 68 + 5204 + 5, scan_count);
+
+		write(7 + 98148 + 68 + 5204 + 6, hist[0].size());
+		write(7 + 98148 + 68 + 5204 + 7, hist[1].size());
+		write(7 + 98148 + 68 + 5204 + 8, hist[2].size());
+
+//		write(7 + 98148 + 68 + 5204 + 4, ret_count);
+//		write(7 + 98148 + 68 + 5204 + 4, trip.second.weekday);
 
 		size += (writed_cnt);
 		out_indptr.write(reinterpret_cast<char*>(&size), sizeof(size));
