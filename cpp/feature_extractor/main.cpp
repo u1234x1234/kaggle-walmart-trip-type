4c14624ec66b18633cf9d223b494861577b9b172
u1234x1234 <u1234x1234@gmail.com>
2015-12-16 04:16:08 +0500

    50it 0.79

--- cpp/feature_extractor/main.cpp
+++ cpp/feature_extractor/main.cpp
@@ -8,160 +8,193 @@
 using namespace std;
 using namespace cv;
 
-string root_path = "/home/linux12341234/kaggle/walmart/";
+string root_path = "/home/dima/walmart/";
 unordered_map<string, int> feature_to_id[6];
 vector<int> feat_count(6);
 
 void read_test_stat()
 {
-    vector<string> header;
-    string line;
-    ifstream in2(root_path + "/prep_test.csv");
-    getline(in2, line);
-    boost::split(header, line, boost::is_any_of("_"));
-    while(getline(in2, line))
-    {
-        vector<string> tokens;
-        boost::split(tokens, line, boost::is_any_of("_"));
-        for (int i = 0; i < 6; i++)
-        {
-            if (feature_to_id[i].find(tokens[i]) == feature_to_id[i].end())
-                feature_to_id[i][tokens[i]] = feat_count[i]++;
-        }
-    }
-    for (int i = 0; i < 6; i++)
-        cout << header[i] << " " << feature_to_id[i].size() << endl;
-    cout << endl;
+	vector<string> header;
+	string line;
+	ifstream in2(root_path + "/prep_test.csv");
+	getline(in2, line);
+	boost::split(header, line, boost::is_any_of("_"));
+	while(getline(in2, line))
+	{
+		vector<string> tokens;
+		boost::split(tokens, line, boost::is_any_of("_"));
+		for (int i = 0; i < 6; i++)
+		{
+			if (feature_to_id[i].find(tokens[i]) == feature_to_id[i].end())
+			{
+				if (tokens[i] == "")
+					cout << "nan: " << i << " " << feat_count[i] << endl;
+				feature_to_id[i][tokens[i]] = feat_count[i]++;
+			}
+		}
+	}
+	for (int i = 0; i < 6; i++)
+		cout << header[i] << " " << feature_to_id[i].size() << endl;
+	cout << endl;
 }
 
 void read_train_stat()
 {
-    vector<string> header;
-    string line;
-    ifstream in2(root_path + "/prep_train.csv");
-    getline(in2, line);
-    boost::split(header, line, boost::is_any_of("_"));
-    while(getline(in2, line))
-    {
-        vector<string> tokens;
-        boost::split(tokens, line, boost::is_any_of("_"));
-        for (int i = 0; i < 6; i++)
-        {
-            if (feature_to_id[i].find(tokens[i+1]) == feature_to_id[i].end())
-                feature_to_id[i][tokens[i+1]] = feat_count[i]++;
-        }
-    }
-    for (int i = 0; i < 6; i++)
-        cout << header[i+1] << " " << feature_to_id[i].size() << endl;
-    cout << endl;
+	vector<string> header;
+	string line;
+	ifstream in2(root_path + "/prep_train.csv");
+	getline(in2, line);
+	boost::split(header, line, boost::is_any_of("_"));
+	while(getline(in2, line))
+	{
+		vector<string> tokens;
+		boost::split(tokens, line, boost::is_any_of("_"));
+		for (int i = 0; i < 6; i++)
+		{
+			if (feature_to_id[i].find(tokens[i+1]) == feature_to_id[i].end())
+			{
+				feature_to_id[i][tokens[i+1]] = feat_count[i]++;
+			}
+		}
+	}
+	for (int i = 0; i < 6; i++)
+		cout << header[i+1] << " " << feature_to_id[i].size() << endl;
+	cout << endl;
 }
 
 struct trip_info
 {
-    int weekday;
-    int trip_type;
-    vector<int> data[4];
+	int weekday;
+	int trip_type;
+	vector<int> data[4];
 };
 
 
 map<int,trip_info> read_trips(string mode = "train")
 {
-    map<int,trip_info> trips;
-    ifstream in(root_path + "/prep_" + mode + ".csv");
-    int fl = (mode == "test");
-    string line;
-    getline(in, line);
-    while(getline(in, line))
-    {
-        vector<string> tokens;
-        boost::split(tokens, line, boost::is_any_of("_"));
-        int trip_type = stoi(tokens[0]);
-        int visit_number = stoi(tokens[1 - fl]);
+	map<int,trip_info> trips;
+	ifstream in(root_path + "/prep_" + mode + ".csv");
+	int fl = (mode == "test");
+	string line;
+	getline(in, line);
+	while(getline(in, line))
+	{
+		vector<string> tokens;
+		boost::split(tokens, line, boost::is_any_of("_"));
+		int trip_type = stoi(tokens[0]);
+		int visit_number = stoi(tokens[1 - fl]);
 
-        int weekday = feature_to_id[1][tokens[2 - fl]];
+		int weekday = feature_to_id[1][tokens[2 - fl]];
 
-        int dat[4];
-        for (int i = 2; i < 6; i++)
-        {
-            if (feature_to_id[i].find(tokens[i + 1 - fl]) == feature_to_id[i].end())
-                continue;
-            dat[i - 2] = feature_to_id[i][tokens[i + 1 - fl]];
-        }
+		int dat[4];
+		for (int i = 2; i < 6; i++)
+		{
+			if (feature_to_id[i].find(tokens[i + 1 - fl]) == feature_to_id[i].end())
+				continue;
+			if (i != 3)// scan_count is already in num format
+				dat[i - 2] = feature_to_id[i][tokens[i + 1 - fl]];
+			else
+			{
+				dat[i - 2] = stoi(tokens[i + 1 - fl]);
+			}
+		}
 
-        auto &trip = trips[visit_number];
-        trip.weekday = weekday;
-        trip.trip_type = trip_type;
-        for (int i = 0; i < 4; i++)
-            trip.data[i].push_back(dat[i]);
-    }
-    return trips;
+		auto &trip = trips[visit_number];
+		trip.weekday = weekday;
+		trip.trip_type = trip_type;
+		for (int i = 0; i < 4; i++)
+			trip.data[i].push_back(dat[i]);
+	}
+	return trips;
 }
 
 void gen_features(const map<int,trip_info> &trips, string mode = "train")
 {
-    ofstream out_indices(root_path + "/" + mode +".indices");
-    ofstream out_indptr(root_path + "/" + mode + ".indptr");
-    ofstream out_data(root_path + "/" + mode + ".data");
-    ofstream out_y(root_path + "/" + mode + ".y");
-    ofstream out(root_path + "/target_mapping");
+	ofstream out_indices(root_path + "/" + mode +".indices");
+	ofstream out_indptr(root_path + "/" + mode + ".indptr");
+	ofstream out_data(root_path + "/" + mode + ".data");
+	ofstream out_y(root_path + "/" + mode + ".y");
+	ofstream out(root_path + "/target_mapping");
 
-    int size = 0;
-    out_indptr.write(reinterpret_cast<char*>(&size), sizeof(size));
-    for (auto &trip : trips)
-    {
-        int idx = trip.second.weekday;
-        int val = 1;
-        out_indices.write(reinterpret_cast<char*>(&idx), sizeof(idx));
-        out_data.write(reinterpret_cast<char*>(&val), sizeof(val));
+	int size = 0;
+	out_indptr.write(reinterpret_cast<char*>(&size), sizeof(size));
+	for (auto &trip : trips)
+	{
+		int writed_cnt = 0;
+		auto write = [&](int idx, int val) -> void
+		{
+			out_indices.write(reinterpret_cast<char*>(&idx), sizeof(idx));
+			out_data.write(reinterpret_cast<char*>(&val), sizeof(val));
+			writed_cnt++;
+		};
+		write(trip.second.weekday, 1);
 
-        for (size_t i = 0; i < trip.second.data[0].size(); i++)
-        {
-            idx = trip.second.data[3][i] + 7;
-            val = trip.second.data[0][i];
-            out_indices.write(reinterpret_cast<char*>(&idx), sizeof(idx));
-            out_data.write(reinterpret_cast<char*>(&val), sizeof(val));
+		int ret_flag = 0;
+		int nan_flag = 0;
+		int scan_count = 0;
 
-            idx = trip.second.data[2][i] + 7 + 5204;
-            val = trip.second.data[0][i];
-            out_indices.write(reinterpret_cast<char*>(&idx), sizeof(idx));
-            out_data.write(reinterpret_cast<char*>(&val), sizeof(val));
-        }
+		map<int, int> hist[3];
 
-        size += (1 + 2*trip.second.data[0].size());
-        out_indptr.write(reinterpret_cast<char*>(&size), sizeof(size));
+		for (size_t i = 0; i < trip.second.data[0].size(); i++)
+		{
+			if (trip.second.data[1][i] < 0) // return
+			{
+				ret_flag++;
+			}
+			else
+			{
+				hist[0][trip.second.data[0][i] + 7] += trip.second.data[1][i];
+				hist[1][trip.second.data[2][i] + 7 + 98148]++;
+				hist[2][trip.second.data[3][i] + 7 + 98148 + 68]++;
+				scan_count += trip.second.data[1][i];
+			}
 
-        int trip_type = trip.second.trip_type;
-//        if (mode == "train")
-        {
-//            cout << trip_type << endl;
-//            exit(0);
-        }
-        out_y.write(reinterpret_cast<char*>(&trip_type), sizeof(trip.second.trip_type));
-    }
+			if (trip.second.data[2][i] == 46)
+				nan_flag++;
+			if (trip.second.data[3][i] == 418)
+				nan_flag++;
+		}
+		for (int i = 0; i < 3; i++)
+			for (auto &it : hist[i])
+			{
+				write(it.first, it.second);
+			}
+
+		write(7 + 98148 + 68 + 5204, ret_flag);
+		write(7 + 98148 + 68 + 5204 + 1, trip.second.data[0].size());
+		write(7 + 98148 + 68 + 5204 + 2, nan_flag);
+		write(7 + 98148 + 68 + 5204 + 3, scan_count);
+
+		size += (writed_cnt);
+		out_indptr.write(reinterpret_cast<char*>(&size), sizeof(size));
+
+		int trip_type = trip.second.trip_type;
+		out_y.write(reinterpret_cast<char*>(&trip_type), sizeof(trip.second.trip_type));
+	}
 }
 
 int main()
 {
-    ifstream in(root_path + "/prep_train.csv");
+	ifstream in(root_path + "/prep_train.csv");
 
-    string line;
-    vector<string> header;
-    getline(in, line);
-    boost::split(header, line, boost::is_any_of("_"));
+	string line;
+	vector<string> header;
+	getline(in, line);
+	boost::split(header, line, boost::is_any_of("_"));
 
-    read_test_stat();
-//    read_train_stat();
-    map<int,trip_info> train_trips = read_trips();
-    map<int,trip_info> test_trips = read_trips("test");
+	read_test_stat();
+	//    read_train_stat();
+	map<int,trip_info> train_trips = read_trips();
+	map<int,trip_info> test_trips = read_trips("test");
 
-    cout << "train_trip size: " << train_trips.size() << endl;
-    cout << "test_trip size: " << test_trips.size() << endl;
+	cout << "train_trip size: " << train_trips.size() << endl;
+	cout << "test_trip size: " << test_trips.size() << endl;
 
-    gen_features(train_trips);
-    gen_features(test_trips, "test");
+	gen_features(train_trips);
+	gen_features(test_trips, "test");
 
 
-    return 0;
+	return 0;
 }
 
 
--- train_wal_sam.py
+++ train_wal_sam.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn import grid_search, metrics, cross_validation, neighbors, pipeline, preprocessing, svm, ensemble, linear_model
 import numpy as np
-import matplotlib.pyplot as plt
 import xgboost as xgb
 from scipy import sparse
 
@@ -26,29 +25,35 @@ y = lbl_enc.fit_transform(y)
 DX = xgb.DMatrix(sp, label=y)
 
 params = {'booster':'gbtree',
-     'max_depth':4,
-#     'min_child_weight':1,
-#     'eta':0.02,
+     'max_depth':8,
+#     'min_child_weight':4,
+     'eta':0.3,
 #     'gamma':0.25,
      'silent':1,
      'objective':'multi:softprob',
 #     'lambda':1.5,
 #     'alpha':1.0,
 #      'lambda_bias':0.5,
-     'nthread':4,
+     'nthread':8,
 #      'max_delta_step': 1,
-#     'subsample':0.5,
+     'subsample':0.9,
      'num_class':38,
-#      'colsample_bytree':0.9,
+      'colsample_bytree':0.9,
      'eval_metric':'mlogloss'
      }
  
-#xgb.cv(params=params, dtrain=DX, nfold=2, show_progress=True, num_boost_round=20)
+xgb.cv(params=params, dtrain=DX, nfold=2, show_progress=True, num_boost_round=100)
 
-clf = xgb.sklearn.XGBClassifier(max_depth=5, objective='multi:softprob', n_estimators=30)
-clf.fit(sp, y, eval_metric='mlogloss')
-preds = clf.predict_proba(sp_test)
+#clf = xgb.sklearn.XGBClassifier(max_depth=5, objective='multi:softprob', n_estimators=30)
+bst = xgb.Booster(params, [DX])
+for i in range(80):
+    bst.update(DX, i)
+    print ('teration: ', i)
 
+#clf.fit(sp, y, eval_metric='mlogloss')
+#preds = clf.predict_proba(sp_test)
+DT = xgb.DMatrix(sp_test)
+preds = bst.predict(DT)
 
 print (preds.shape)
 sub = pd.read_csv('sample_submission.csv')
